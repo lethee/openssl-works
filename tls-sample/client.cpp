1d93b018c1cdf87e05ea11c59b22ed8992111ca6
Seonghwan Lee <sean.lee@lge.com>
2016-01-27 13:51:25 +0900

    update code and indent

--- tls-sample/client.cpp
+++ tls-sample/client.cpp
@@ -13,139 +13,139 @@
 using namespace std;
 
 class Client {
-  unsigned long ssl_err = 0;
-  SSL_CTX* ctx = nullptr;
+	unsigned long ssl_err = 0;
+	SSL_CTX* ctx = nullptr;
 
 public:
-  void init() {
-    ctx = SSL_CTX_new(TLSv1_method());
-    ssl_err = ERR_get_error();
-    if(!(ctx != nullptr)) {
-      cerr << ssl_err << " [Error] SSL_CTX_new" << endl;
-      return; /* failed */
-    }
-  }
+	void init() {
+		ctx = SSL_CTX_new(TLSv1_method());
+		ssl_err = ERR_get_error();
+		if(!(ctx != nullptr)) {
+			cerr << ssl_err << " [Error] SSL_CTX_new" << endl;
+			return; /* failed */
+		}
+	}
 
-  void connect(const char *host, const char * port) {
-    BIO *web = nullptr;
-    long res = 1;
-    SSL *ssl = nullptr;
-    BIO *out = nullptr;
-    char hostname[128];
+	void connect(const char *host, const char * port) {
+		BIO *web = nullptr;
+		long res = 1;
+		SSL *ssl = nullptr;
+		BIO *out = nullptr;
+		char hostname[128];
 
-    cout << "BIO_new_ssl_connect" << endl;
-    web = BIO_new_ssl_connect(ctx);
-    ssl_err = ERR_get_error();
-    if(!(web != nullptr))
-    {
-      cerr << ssl_err << " [Error] BIO_new_ssl_connect" << endl;
-      return; /* failed */
-    }
+		cout << "BIO_new_ssl_connect" << endl;
+		web = BIO_new_ssl_connect(ctx);
+		ssl_err = ERR_get_error();
+		if(!(web != nullptr))
+		{
+			cerr << ssl_err << " [Error] BIO_new_ssl_connect" << endl;
+			return; /* failed */
+		}
 
-    cout << "BIO_set_conn_hostname" << endl;
-    snprintf(hostname, sizeof(hostname), "%s:%s", host, port);
-    res = BIO_set_conn_hostname(web, hostname);
-    ssl_err = ERR_get_error();
-    if(!(1 == res))
-    {
-      cerr << ssl_err << " [Error] BIO_set_conn_hostname" << endl;
-      return; /* failed */
-    }
+		cout << "BIO_set_conn_hostname" << endl;
+		snprintf(hostname, sizeof(hostname), "%s:%s", host, port);
+		res = BIO_set_conn_hostname(web, hostname);
+		ssl_err = ERR_get_error();
+		if(!(1 == res))
+		{
+			cerr << ssl_err << " [Error] BIO_set_conn_hostname" << endl;
+			return; /* failed */
+		}
 
-    cout << "BIO_get_ssl" << endl;
-    BIO_get_ssl(web, &ssl);
-    ssl_err = ERR_get_error();
-    if(!(ssl != nullptr))
-    {
-      cerr << ssl_err << " [Error] BIO_get_ssl" << endl;
-      return; /* failed */
-    }
+		cout << "BIO_get_ssl" << endl;
+		BIO_get_ssl(web, &ssl);
+		ssl_err = ERR_get_error();
+		if(!(ssl != nullptr))
+		{
+			cerr << ssl_err << " [Error] BIO_get_ssl" << endl;
+			return; /* failed */
+		}
 
-    cout << "SSL_set_tlsext_host_name" << endl;
-    res = SSL_set_tlsext_host_name(ssl, host);
-    ssl_err = ERR_get_error();
-    if(!(1 == res))
-    {
-      cerr << ssl_err << " [Error] ERR_get_error" << endl;
-      return; /* failed */
-    }
+		cout << "SSL_set_tlsext_host_name" << endl;
+		res = SSL_set_tlsext_host_name(ssl, host);
+		ssl_err = ERR_get_error();
+		if(!(1 == res))
+		{
+			cerr << ssl_err << " [Error] ERR_get_error" << endl;
+			return; /* failed */
+		}
 
-    out = BIO_new_fp(stdout, BIO_NOCLOSE);
-    ssl_err = ERR_get_error();
-    if(!(out != nullptr))
-    {
-      cerr << ssl_err << " [Error] BIO_new_fp" << endl;
-      return; /* failed */
-    }
+		out = BIO_new_fp(stdout, BIO_NOCLOSE);
+		ssl_err = ERR_get_error();
+		if(!(out != nullptr))
+		{
+			cerr << ssl_err << " [Error] BIO_new_fp" << endl;
+			return; /* failed */
+		}
 
-    cout << "BIO_do_connect" << endl;
-    res = BIO_do_connect(web);
-    ssl_err = ERR_get_error();
-    if(!(1 == res))
-    {
-      cerr << ssl_err << " [Error] BIO_do_connect" << endl;
-      return; /* failed */
-    }
+		cout << "BIO_do_connect" << endl;
+		res = BIO_do_connect(web);
+		ssl_err = ERR_get_error();
+		if(!(1 == res))
+		{
+			cerr << ssl_err << " [Error] BIO_do_connect" << endl;
+			return; /* failed */
+		}
 
-    cout << "BIO_do_handshake" << endl;
-    res = BIO_do_handshake(web);
-    ssl_err = ERR_get_error();
-    if(!(1 == res))
-    {
-      cerr << ssl_err << " [Error] BIO_do_handshake" << endl;
-      return; /* failed */
-    }
+		cout << "BIO_do_handshake" << endl;
+		res = BIO_do_handshake(web);
+		ssl_err = ERR_get_error();
+		if(!(1 == res))
+		{
+			cerr << ssl_err << " [Error] BIO_do_handshake" << endl;
+			return; /* failed */
+		}
 
-    const SSL_SESSION *session = SSL_get_session(ssl);
-    if (session != nullptr) {
-      SSL_SESSION_print(out, session);
-    }
+		const SSL_SESSION *session = SSL_get_session(ssl);
+		if (session != nullptr) {
+			SSL_SESSION_print(out, session);
+		}
 
-    // Certificate Verification Here
+		// Certificate Verification Here
 
-    X509* cert = SSL_get_peer_certificate(ssl);
-    if(cert) {
-      X509_print(out, cert);
-      X509_free(cert);
-    }
+		X509* cert = SSL_get_peer_certificate(ssl);
+		if(cert) {
+			X509_print(out, cert);
+			X509_free(cert);
+		}
 
-    // SSL_get_verify_result(ssl);
+		// SSL_get_verify_result(ssl);
 
-    // Contents
+		// Contents
 
-    BIO_puts(web, "GET " "/" " HTTP/1.1\r\nHost: ");
-    BIO_puts(web, hostname);
-    BIO_puts(web, "\r\nConnection: close\r\n\r\n");
-    BIO_puts(out, "\nFetching: " "/" "\n\n");
+		BIO_puts(web, "GET " "/" " HTTP/1.1\r\nHost: ");
+		BIO_puts(web, hostname);
+		BIO_puts(web, "\r\nConnection: close\r\n\r\n");
+		BIO_puts(out, "\nFetching: " "/" "\n\n");
 
-    int len = 0;
-    do {
-      char buff[1536] = {};
-      len = BIO_read(web, buff, sizeof(buff));
-      if (len > 0) {
-        BIO_write(out, buff, len);
-      }
-    } while (len > 0 || BIO_should_retry(web));
+		int len = 0;
+		do {
+			char buff[1536] = {};
+			len = BIO_read(web, buff, sizeof(buff));
+			if (len > 0) {
+				BIO_write(out, buff, len);
+			}
+		} while (len > 0 || BIO_should_retry(web));
 
-    if (out)
-        BIO_free(out);
+		if (out)
+			BIO_free(out);
 
-    if (web != nullptr)
-        BIO_free_all(web);
+		if (web != nullptr)
+			BIO_free_all(web);
 
-    if (nullptr != ctx)
-        SSL_CTX_free(ctx);
-  }
+		if (nullptr != ctx)
+			SSL_CTX_free(ctx);
+	}
 };
 
 int main(int argc, char *argv[]) {
-  SSL_library_init();
+	SSL_library_init();
 
-  Client client;
-  client.init();
-  client.connect("www.google.com", "443");
+	Client client;
+	client.init();
+	client.connect("www.google.com", "443");
 
-  Client client2;
-  client2.init();
-  client2.connect("localhost", "4433");
+	Client client2;
+	client2.init();
+	client2.connect("localhost", "4433");
 }
--- tls-sample/server.cpp
+++ tls-sample/server.cpp
@@ -16,7 +16,6 @@ using namespace std;
 class Server {
 	unsigned long ssl_err = 0;
 	SSL_CTX *ctx;
-	int port = 4433;
 	int sock = 0;
 
 public:
@@ -29,40 +28,38 @@ public:
 		}
 	}
 
-	void initCert() {
-		// Self-signed Certificate
-		// $ openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -subj “/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=www.example.com"
+	void initCert(const char *cert, const char *key) {
 		int res = 0;
 
-		res = SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM);
+		res = SSL_CTX_use_certificate_file(ctx, cert, SSL_FILETYPE_PEM);
 		if (res != 1) { cerr << res << " [Error] SSL_CTX_use_certificate_file" << endl; return; }
 
-		res = SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM);
+		res = SSL_CTX_use_PrivateKey_file(ctx, key, SSL_FILETYPE_PEM);
 		if (res != 1) { cerr << res << " [Error] SSL_CTX_use_PrivateKey_file" << endl; return; }
 
 		res = SSL_CTX_check_private_key(ctx);
 		if (res != 1) { cerr << res << " [Error] SSL_CTX_check_private_key" << endl; return; }
 	}
 
-	void doListen() {
+	void doListen(int port) {
 		struct sockaddr_in addr;
 		bzero(&addr, sizeof(addr));
 
 		sock = socket(PF_INET, SOCK_STREAM, 0);
 
 		addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = INADDR_ANY;
+		addr.sin_port = htons(port);
+		addr.sin_addr.s_addr = INADDR_ANY;
 
-    if ( bind(sock, (struct sockaddr*)&addr, sizeof(addr)) != 0 ) {
-        perror("can't bind port");
-        return;
-    }
+		if ( bind(sock, (struct sockaddr*)&addr, sizeof(addr)) != 0 ) {
+			perror("can't bind port");
+			return;
+		}
 
-    if ( listen(sock, 10) != 0 ) {
-        perror("Can't configure listening port");
-        return;
-    }
+		if ( listen(sock, 10) != 0 ) {
+			perror("Can't configure listening port");
+			return;
+		}
 	}
 
 	void doRead() {
@@ -123,8 +120,8 @@ int main(int argc, char *argv[])
 
 	Server server;
 	server.initSSL();
-	server.initCert();
-	server.doListen();
+	server.initCert("cert.pem", "key.pem");
+	server.doListen(4433);
 	server.doRead();
 	server.doClose();
 
